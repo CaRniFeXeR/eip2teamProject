de770c5e21786583f8daff37cad1319f3623fc71
fabs04 <nagler.fabian@gmail.com>
2018-05-08 14:19:22 +0200

    LocationList: separate ArrayLists for Airport and Trainstations, Methods adapted

--- src/LocationList.java
+++ src/LocationList.java
@@ -1,54 +1,67 @@
 import Models.Airport;
 import Models.Location;
+import Models.Trainstation;
 
 import java.util.ArrayList;
 
 public class LocationList implements Datastructure {
-    private ArrayList<Location> locations = new ArrayList<>();
+    private ArrayList<Airport> airports = new ArrayList<>();
+    private ArrayList<Trainstation> trainstations = new ArrayList<>();
 
     @Override
     public void add(Location location) {
-        this.locations.add(location);
+       if(location.getClass() == Airport.class){
+           this.airports.add((Airport) location);
+       }else{
+           this.trainstations.add((Trainstation) location);
+       }
     }
 
     @Override
     public int[] countLocationsInArea(double x, double y, double r) {
-        int airports = 0;
-        int trainstations = 0;
+        int airportCount = 0;
+        int trainstationCount = 0;
 
-        for(int i = 0; i < this.locations.size(); i++){
-            double dx = this.locations.get(i).getXCoordiante() - x;
-            double dy = this.locations.get(i).getYCoordinate() - y;
+        for(int i = 0; i < this.airports.size(); i++){
+            double dx = this.airports.get(i).getXCoordiante() - x;
+            double dy = this.airports.get(i).getYCoordinate() - y;
             double distance = Math.abs(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
 
-            if (distance <= r){
-                 if(this.locations.get(i).getClass() == Airport.class) {
-                    airports++;
-                }else{
-                    trainstations++;
-                }
+            if(distance <= r){
+                airportCount++;
             }
-
         }
 
-        return new int[] {airports, trainstations};
+
+        for(int i = 0; i < this.trainstations.size(); i++){
+            double dx = this.trainstations.get(i).getXCoordiante() - x;
+            double dy = this.trainstations.get(i).getYCoordinate() - y;
+            double distance = Math.abs(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
+
+            if(distance <= r){
+                trainstationCount++;
+            }
+        }
+
+
+        return new int[] {airportCount, trainstationCount};
     }
 
     @Override
     public int countAriportsNearTrainstation(double r, int n) {
-        int airports = 0;
+        int airportCount = 0;
 
-        for(int i = 0; i < this.locations.size(); i++){
-            if(this.locations.get(i).getClass() == Airport.class){
-                double x = this.locations.get(i).getXCoordiante();
-                double y = this.locations.get(i).getYCoordinate();
+        for(int i = 0; i < this.airports.size(); i++){
 
-                if(this.countLocationsInArea(x, y, r)[1] >= n){
-                    airports++;
-                }
+            double x = this.airports.get(i).getXCoordiante();
+            double y = this.airports.get(i).getYCoordinate();
+
+            if(this.countLocationsInArea(x, y, r)[1] >= n){
+                airportCount++;
             }
+
         }
 
-        return airports;
+        return airportCount;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import Models.Location;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("TEAMAUFGABE");
         System.out.println("TEAMAUFGABE");
         System.out.println("Fabs initialized!");
 
-        DataReader.ReadData();
+        LocationList list = new LocationList();
+        DataReader.ReadData(list);
+        System.out.println(list.countLocationsInArea(0.0, 0.0, 78055556)[1]);
     }
 }
