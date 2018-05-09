e75764818eee887f24b854fe5bda6386ffa6b601
CaRniFeXeR <flo.kowarsch@yahoo.de>
2018-05-09 16:25:06 +0200

    //kleine Anpasung in Main

--- src/ep2/teamaufgabe/Main.java
+++ src/ep2/teamaufgabe/Main.java
@@ -13,30 +13,21 @@ public class Main {
         ArrayList<Datastructure> datastructures = new ArrayList<>();
 
 
-        datastructures.add(new KDTreeStructure());
         datastructures.add(new Quadtree(-81918465352051400d, 81918465352051400d, -59720745659516000d, 59720745659516000d));
-        // datastructures.add(new LocationList());
+        datastructures.add(new KDTreeStructure());
+        //add Simplelist to compare
+        //datastructures.add(new LocationList());
+
 
         Tester test = new Tester();
 
-        test.testAndComparePerofrmance(datastructures, "junctions.csv", 4, true);
+       /* TestInput[] instructsToTest = new TestInput[1];
+        instructsToTest[0] = new TestInput(25.24419229708292, 19);
+        test.testAndComparePerformance(datastructures, instructsToTest, "junctions.csv", true);
+        */
 
-        // DataReader.ReadData(twoDimensionTree,"junctions.csv");
-
-        // twoDimensionTree.print();
-
-        //System.out.println(twoDimensionTree.countLocationsInArea(0,0,2)[1]);
-        //  System.out.println("Number of Airports with n Trainsatation in an Distance of 15 " + twoDimensionTree.countAriportsNearTrainstation(1,5));
-        //System.out.println(twoDimensionTree.countLocationsInArea(0,0,10)[0]);
+        test.testAndComparePerofrmance(datastructures, "junctions.csv", 400, true);
 
 
-        //Simple Data
-        //DataReader.ReadData(listStucture,"simpleData.csv");
-
-        //big Data
-        //DataReader.ReadData(listStucture,"junctions.csv");
-
-        //System.out.println("Number of Trainstations :" + listStucture.countLocationsInArea(0,0,6)[1]);
-        //System.out.println("Number of Airports with n Trainsatation in an Distance of 1 " + listStucture.countAriportsNearTrainstation(1,5));
     }
 }
